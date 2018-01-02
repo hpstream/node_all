56bab6e6bd3be508654c47709e091e986333ba07
peng.huang <peng.huang@future-data.cn>
2018-01-02 10:39:01 +0800

    基础全局变量总结

--- global/global.js
+++ global/global.js
@@ -1,4 +1,53 @@
 
 /**
- * Created by hp on 2018-01-02.
+ * 全局变量的练习
  */
+
+
+// 1. console 与在浏览器使用的方式是一样的
+
+//console.log(1);
+
+
+// 2. __filename  拿到具体文件的路径（包括文件名称）
+console.log(__filename);  //C:\Users\10918\Documents\node_all\global\global.js
+
+
+
+// 3. __dirname 拿到具体的文件路径（不包括具体名称）
+
+console.log(__dirname);//C:\Users\10918\Documents\node_all\global
+
+// 4. setImmediate, setInterval, setTimeout
+/**
+ * setInterval
+ * setImmediate 与 setTimeout(callback,0)一样
+ */
+
+
+// 5.clearImmediate, clearTimeout, clearInterval 与浏览器用法一样
+
+
+// 6.process 事件(beforeExit, disconnect, exit, messagge) 感觉不常用, signal Events 这个事件好像与子进程同行有关。没有深入研究
+//process.on('exit',(code)=>{console.log(code+100)});
+
+// setTimeout(()=>{ process.exit()},1000)
+
+// 7.process 方法（chdir，cwd）
+
+process.chdir('../') //更改当前的目录
+console.log(process.cwd()); //获取当前的目录
+
+
+
+
+// 8.process 属性(arch,argv,paltform)
+console.log(process.arch) //可以拿到计算机处理器架构，如：'x64','ia32','arm'
+
+console.log(process.argv) //可以拿到命令行输入的参数
+
+/**
+ * 进程中还有许多属性与方法，在这里不一一介绍用法，记住他可以拿到进程中所以要的相关参数即可。
+ */
+
+
--- global/global.md
+++ global/global.md
@@ -1,1 +1,14 @@
-在nodejs环境中，我们有直接可以拿到的全局变量
+** 在nodejs环境中，我们有直接可以拿到的全局变量（在相同的路径下，有对应的jsdemo） **
+
+### 常用的全局变量如下： ###
+1. __dirname   ---->  拿到具体文件的路径（包括文件名称）
+2. __filename  ---->  拿到具体的文件路径（不包括具体名称）
+3. setImmediate(callback[,...args])
+4. setInterval(callback[,...args])
+5. setTimeout(callback,[,...args])
+6. clearImmediate(immediateObject)
+7. clearInterval(intervalObject)
+8. clearTimeout(timeoutObject)
+9. console
+10. process （全局的进程对象，拥有自己的属性和方法）
+11. reqire() (导入其他的js)
