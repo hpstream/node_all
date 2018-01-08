de33839af8e355bb419122979fcb13673d99cdcc
peng.huang <peng.huang@future-data.cn>
2018-01-08 18:04:07 +0800

    socket.io基础服务完成

---
+++ myExercise/websocket/server.js
@@ -0,0 +1,44 @@
+/**
+ * 使用socket.io 完成及时通讯
+ */
+
+let express = require('express');
+let app = express();
+//
+// //添加静态文件
+app.use('/',express.static('./static'));
+//
+//
+app.get('/',(req,res) =>{
+	res.sendFile(__dirname + '/static/client.html');
+})
+//
+//
+var server = require('http').createServer(app);
+let io = require('socket.io')(server);
+
+var clients = [];
+io.on("connection", function(socket){
+	clients.push(socket);
+	socket.send({count:clients.length});
+	socket.on('message',function(data){
+		//收到消息
+		for(var i=0;i<clients.length;i++){
+			clients[i].send({count:clients.length,info:data});
+		}
+
+	});
+	socket.on('disconnect',function () {
+		clients = clients.splice(clients.indexOf(socket), 1)
+		console.log(`${socket.id} 离开了`);
+
+
+	})
+});
+
+
+
+server.listen(80);
+
+
+
