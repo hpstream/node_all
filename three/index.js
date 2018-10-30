34e6389329cfb104f78d072a913983d53e8c95f6
huangpeng <huangpeng@moqipobing.com>
2018-10-30 18:08:40 +0800

    完成3d模型

--- three/index.js
+++ three/index.js
@@ -6,11 +6,12 @@
  */
 
 
-var renderer, camera, scene, light,stats;
+var renderer, camera, scene, light, stats;
 function initThree() {
     // 创建一个 renderer渲染器
     renderer = new THREE.WebGLRenderer();
-    renderer.setSize(800, 600);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor('rgba(255,255,255,1)', 1.0);
     // 相当于把这个canvas 放到body里面去
     document.getElementsByTagName('body')[0].appendChild(renderer.domElement);
     // 增加帧数
@@ -32,35 +33,44 @@ function initScene() {
 function initCamera() {
     // 创建一个相机
     camera = new THREE.PerspectiveCamera(45, 4 / 3, 1, 1000);
-    camera.position.set(0, 0, 100);
+    camera.position.set(0, 0, 0);
 }
 
 function initLight() {
     // 创建一个灯光
-    light = new THREE.DirectionalLight(0xFF0000, 1.0, 0);
+    light = new THREE.DirectionalLight(0xffffff, 1.0, 1);
     light.position.set(100, 100, 200);
     scene.add(light);
 }
 // 创建一个材料
 var cube, timer = 0.01;
+
+
+var rouletteScene;
 function initObject() {
 
-    var geometry = new THREE.Geometry();
-    geometry.vertices.push(new THREE.Vector3(0, -20, 0));
-    geometry.vertices.push(new THREE.Vector3(0, 20, 0));
+    // var geometry = new THREE.Geometry();
+    // geometry.vertices.push(new THREE.Vector3(0, -20, 0));
+    // geometry.vertices.push(new THREE.Vector3(0, 20, 0));
 
-    for (var i = 0; i <= 20; i++) {
+    // for (var i = 0; i <= 20; i++) {
 
-        var line = new THREE.Line(geometry, new THREE.LineBasicMaterial({ color: 0xff0000, opacity: 1 }));
-        line.position.x = (i * 5) - 50;
-        scene.add(line);
+    //     var line = new THREE.Line(geometry, new THREE.LineBasicMaterial({ color: 0xff0000, opacity: 1 }));
+    //     line.position.x = (i * 5) - 50;
+    //     scene.add(line);
 
-        var line = new THREE.Line(geometry, new THREE.LineBasicMaterial({ color: 0xff0000, opacity: 1 }));
-        line.position.y = (i * 5) - 50;
-        line.rotation.z = 90 * Math.PI / 180;
-        scene.add(line);
-
-    }
+    //     var line = new THREE.Line(geometry, new THREE.LineBasicMaterial({ color: 0xff0000, opacity: 1 }));
+    //     line.position.y = (i * 5) - 50;
+    //     line.rotation.z = 90 * Math.PI / 180;
+    //     scene.add(line);
+    // }
+    var loader = new THREE.ColladaLoader();
+    loader.load("./model/model.dae", function (collada) {
+        rouletteScene = collada.scene;
+        rouletteScene.position.set(-4, -4, -40);
+        rouletteScene.rotation.set(-90 * Math.PI / 180, 0, 0);
+        scene.add(rouletteScene);
+    })
 }
 // 添加材料贴图
 
@@ -83,13 +93,77 @@ function threeStart() {
     // renderer.clear();
     animation();
 }
-
+var isdown = false;
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+var x0 = 0, y0 = 0, x1 = 0, y1 = 0, x = 0, y = 0;
 function animation() {
     //renderer.clear();
-    // camera.rotation.y = camera.rotation.y + 0.01;
+    if (rouletteScene) {
+        // rouletteScene.rotation.x = x1;
+        // rouletteScene.rotation.y = y1;
+
+    }
+
+    // update the picking ray with the camera and mouse position
+
     renderer.render(scene, camera);
     requestAnimationFrame(animation);
     stats.update();
 }
 
 threeStart();
+
+
+
+function onMouseMove(event) {
+
+    // calculate mouse position in normalized device coordinates
+    // (-1 to +1) for both components
+
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+
+}
+renderer.domElement.addEventListener('mousedown', function (e) {
+    isdown = true;
+    x0 = e.clientX;
+    y0 = e.clientY;
+})
+renderer.domElement.addEventListener('mousemove', function (e) {
+    if (isdown) {
+        x1 = (e.clientX - x0) * Math.PI / 180;
+        y1 = (e.clientY - y0) * Math.PI / 180;
+        // mouse.x = x1;
+        // mouse.y = y1;
+        // camera.position.x = x + x1;
+        // camera.position.y = y + y1;
+
+        //将鼠标点击位置的屏幕坐标转成threejs中的标准坐标,具体解释见代码释义
+        mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+        mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+        //新建一个三维单位向量 假设z方向就是0.5
+        //根据照相机，把这个向量转换到视点坐标系
+        var vector = new THREE.Vector3(mouse.x, mouse.y, 0).unproject(camera);
+
+        //在视点坐标系中形成射线,射线的起点向量是照相机， 射线的方向向量是照相机到点击的点，这个向量应该归一标准化。
+        var raycaster = new THREE.Raycaster(camera.position, vector.sub(camera.position).normalize());
+
+        //射线和模型求交，选中一系列直线
+        var intersects = raycaster.intersectObjects(scene.children);
+        console.log('imtersrcts=' + intersects)
+
+        if (intersects.length > 0) {
+            //选中第一个射线相交的物体
+            SELECTED = intersects[0].object;
+            var intersected = intersects[0].object;
+            console.log(intersects[0].object)
+        }
+
+    }
+})
+renderer.domElement.addEventListener('mouseup', function (e) {
+    isdown = false;
+    x = camera.position.x;
+    y = camera.position.y;
+})
